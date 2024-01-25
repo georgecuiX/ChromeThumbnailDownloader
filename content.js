0094fac68cbbdee7950ad941b725c2d740ddc2eb
George Cui <89486336+georgecuiX@users.noreply.github.com>
2024-01-25 11:27:48 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 /*
  * content.js
  * This JavaScript file is a content script that can interact with web pages that the user visits.
- * Currently, it does not contain any specific functionality but can be used for future enhancements.
+ * Currently, it does not contain any specific functionality will be used for further enhancements
  */
